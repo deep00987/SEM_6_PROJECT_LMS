35ba495b371ec1fb2b60f5ff55a49633702c1847
deep00987 <24670719+deep00987@users.noreply.github.com>
2022-06-26 20:28:22 +0530

    cleaned courses module; added docs

--- api/controllers/courses.js
+++ api/controllers/courses.js
@@ -1,10 +1,20 @@
+/**
+ * @module courses 
+ * Handles users intercations with the cources 
+ */
 const res = require("express/lib/response")
 const db = require("../../database")
 const util = require('util')
-
 const query = util.promisify(db.query).bind(db);
 
+/**
+ * this function maps students with the requestion courses
+ * @param {object} req - thr request object 
+ * @param {object} res - the response object
+ * @returns {object} res with satus code w.r.t operations
+ */
 async function mapStudentCourse(req, res){
+
   const student_id = req.body.client.id
   const course_id = req.body.course_id
   let rows;
@@ -12,29 +22,23 @@ async function mapStudentCourse(req, res){
     INSERT INTO student_course (student_id, course_id)
     VALUES (?, ?);
   `
-
+  //check if the user already joined the course or not 
   try {
-    console.log(student_id, course_id)
-
     rows = await query(`SELECT student_id, course_id FROM student_course WHERE student_id = ${student_id} AND course_id = ${course_id};`)
-    
   } catch (error) {
-    console.log(error)
     return res.status(400).json({
       "success": 0,
       "msg":"cannot enroll into course"
     })
   }
 
-  console.log(rows)
   if(rows.length > 0 && rows[0].student_id === student_id && rows[0].course_id === course_id){
-   
     return res.status(200).json({ 
       "success": 0, 
       "msg": "already enrolled in the following course"
     })
   }
-
+  // map student with the requesting course
   try {
     let result = await query(sql, [student_id, course_id])
     return res.status(200).json({ 
@@ -42,7 +46,6 @@ async function mapStudentCourse(req, res){
       "msg": "Enrolled"
     })
   } catch (error) {
-    console.log(error)
     return res.status(400).json({
       "success": 0,
       "msg":"cannot enroll into course"
@@ -51,14 +54,17 @@ async function mapStudentCourse(req, res){
 
 }
 
-//delete entry from student course 
-
+/**
+ * this function un-maps students with the requestion courses
+ * @param {object} req - thr request object 
+ * @param {object} res - the response object
+ * @returns {object} res with satus code w.r.t operations
+ */
 async function unmapStudentCourse(req, res){
+
   const student_id = req.body.client.id
   const course_id = req.body.course_id
-  
   let rows;
-
   const sql1 = `
     select * from student_course where student_id = ${student_id} AND course_id = ${course_id}
   `
@@ -67,32 +73,28 @@ async function unmapStudentCourse(req, res){
     WHERE student_id = ${student_id} AND 
     course_id = ${course_id};
   `
-
+  //check if the user joined the course or not
   try {
     rows = await query(sql1)
-    console.log(rows)
   } catch (error) {
-    console.log(error)
     return res.status(400).json({
       "success": 0,
       "msg":"something went wrong"
     })
   }
 
-  //check if course requested, exist or not
+  // show error if the requesting course had not been joined by the user
   if(rows.length === 0){
     return res.status(404).json({
       "success": 0,
       "msg": "Enrollment info regarding this course is not found"
     })
   }
-
+  //delete entry from student course 
   try {
     rows = await query(sql2)
-    console.log(rows)
     return res.json({"success": 1, "msg": "opted out off the course sucessfully"})
   } catch (error) {
-    console.log(error)
     return res.status(400).json({
       "success": 0,
       "msg":"something went wrong"
@@ -100,6 +102,5 @@ async function unmapStudentCourse(req, res){
   }
   
 }
-
 
 module.exports = {mapStudentCourse, unmapStudentCourse}
